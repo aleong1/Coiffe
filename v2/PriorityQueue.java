c184b757eebba9168ede49171af647c19d83393b
LilyYan2001 <lyan@stuy.edu>
2018-05-10 08:32:21 -0400

    ???

---
+++ v2/PriorityQueue.java
@@ -0,0 +1,11 @@
+public interface PriorityQueue {
+
+    //~~~~~~~~~~~~ API methods ~~~~~~~~~~~~~~~~~
+
+    public void add(String x);
+    public boolean isEmpty();
+    public String peekMin();
+    public String removeMin();
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+}
