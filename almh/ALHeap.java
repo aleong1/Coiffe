a16ca0cce784b034bdef04117b15ef687106d188
LilyYan2001 <lyan@stuy.edu>
2018-05-14 18:51:58 -0400

    tried tostring

--- almh/ALHeap.java
+++ almh/ALHeap.java
@@ -3,7 +3,7 @@
   APCS2 pd1
   HW49 -- Sink || Swim
   2018-05-14
- */
+*/
 
 /*****************************************************
  * class ALHeap
@@ -27,8 +27,7 @@ public class ALHeap
 	_heap = new ArrayList<Integer>();
     }
 
-
-
+    
     /*****************************************************
      * toString()  ---  overrides inherited method
      * Returns either 
@@ -38,10 +37,31 @@ public class ALHeap
     public String toString() 
     {
 	String s = "";
+	/*
+	int numBranches = 1;
+	
+        for (int start = 0; start < _heap.size(); start = 2*start+1) {
+	    s += "\n";
+	    for (int ctr = start; ctr < _heap.size() && ctr < 2*start+1; ctr ++) {
+		s += _heap.get(ctr) + " ";
+	    }
+	    if (2*start+1 < _heap.size()) {
+		s += "\n" + multiplyStrings(numBranches, "/\\");
+		numBranches *= 2;
+	    }
+	}
+	*/
+
 	for (Integer i: _heap) s += i + " ";
 	return s;
     }//O(?)
 
+    //toString() helper
+    private String multiplyStrings(int n, String s) {
+	String ans = "";
+	for (int ctr = 0; ctr < n; ctr ++) ans += s;
+	return ans;
+    }
 
     /*****************************************************
      * boolean isEmpty()
@@ -89,24 +109,25 @@ public class ALHeap
      *****************************************************/
     public Integer removeMin()   //remove the root
     {
-	if (_heap.size() == 0) return null; //if heap is empty
+	if (_heap.size() == 0) return null; //if size of heap is 0
 	
-        Integer min = _heap.remove(0);
-	
-	if (_heap.size() == 0) return min; //if heap had only one element
-	
-	swap(0, _heap.size()-1);
-        int currentIndex = 0;
-	int childIndex = minChildPos(currentIndex);
-	System.out.println(childIndex);
+	Integer min = new Integer(_heap.remove(0)); //storing val to be removed
 
-	while (childIndex != -1) {
+	if (_heap.size() == 0) return min; //if size was 1, no need to rearrange anything in the heap
+	
+	_heap.add(0, _heap.remove(_heap.size()-1)); //putting last leaf as root
+	int currentIndex = 0;
+	int childIndex = minChildPos(0);
+
+	while (childIndex != -1) { //while current node still has children
 	    if (_heap.get(childIndex).compareTo(_heap.get(currentIndex)) < 0) swap(currentIndex, childIndex);
 	    currentIndex = childIndex;
 	    childIndex = minChildPos(currentIndex);
 	}
 	return min;
-    }//O(?)
+
+	
+    }//O(logn)
 
 
     /*****************************************************
@@ -117,13 +138,12 @@ public class ALHeap
      *****************************************************/
     private int minChildPos( int pos )
     {
-	int leftChild = 2*pos + 1;
+        int leftChild = 2*pos + 1;
 	int rightChild = 2*pos + 2;
-	if (pos >= _heap.size() || 
-	    (leftChild >= _heap.size() && rightChild >= _heap.size())) return -1;
-        if (_heap.get(leftChild).compareTo(_heap.get(rightChild)) <= 0) return leftChild;
+	if (pos >= _heap.size() || rightChild >= _heap.size()) return -1;
+	if (minOf(_heap.get(leftChild), _heap.get(rightChild)).equals(_heap.get(leftChild))) return leftChild;
 	return rightChild;
-    }//O(?)
+    }//O(1)
   
 
     //************ aux helper fxns ***************
@@ -147,53 +167,55 @@ public class ALHeap
     //main method for testing
     public static void main( String[] args )
     {
-	  ALHeap pile = new ALHeap();
+	ALHeap pile = new ALHeap();
 
-	  pile.add(2);
-	  System.out.println(pile);
-	  pile.add(4);
-	  System.out.println(pile);
-	  pile.add(6);
-	  System.out.println(pile);
-	  pile.add(8);
-	  System.out.println(pile);
-	  pile.add(10);
-	  System.out.println(pile);
-	  pile.add(1);
-	  System.out.println(pile);
-	  pile.add(3);
-	  System.out.println(pile);
-	  pile.add(5);
-	  System.out.println(pile);
-	  pile.add(7);
-	  System.out.println(pile);
-	  pile.add(9);
-	  System.out.println(pile);
+	pile.add(2);
+	System.out.println(pile);
+	pile.add(4);
+	System.out.println(pile);
+	pile.add(6);
+	System.out.println(pile);
+	pile.add(8);
+	System.out.println(pile);
+	pile.add(10);
+	System.out.println(pile);
+	pile.add(1);
+	System.out.println(pile);
+	pile.add(3);
+	System.out.println(pile);
+	pile.add(5);
+	System.out.println(pile);
+	pile.add(7);
+	System.out.println(pile);
+	pile.add(9);
+	System.out.println(pile);
 
-	  System.out.println("removing " + pile.removeMin() + "...");
-	  System.out.println(pile);
-	  System.out.println("removing " + pile.removeMin() + "...");
-	  System.out.println(pile);
-	  System.out.println("removing " + pile.removeMin() + "...");
-	  System.out.println(pile);
-	  System.out.println("removing " + pile.removeMin() + "...");
-	  System.out.println(pile);
-	  System.out.println("removing " + pile.removeMin() + "...");
-	  System.out.println(pile);
-	  System.out.println("removing " + pile.removeMin() + "...");
-	  System.out.println(pile);
-	  System.out.println("removing " + pile.removeMin() + "...");
-	  System.out.println(pile);
-	  System.out.println("removing " + pile.removeMin() + "...");
-	  System.out.println(pile);
-	  System.out.println("removing " + pile.removeMin() + "...");
-	  System.out.println(pile);
-	  System.out.println("removing " + pile.removeMin() + "...");
-	  System.out.println(pile);
-	  System.out.println("removing " + pile.removeMin() + "...");
-	  System.out.println(pile);
+	System.out.println("\nremoving " + pile.removeMin() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMin() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMin() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMin() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMin() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMin() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMin() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMin() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMin() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMin() + "...");
+	System.out.println(pile);
+	System.out.println("removing " + pile.removeMin() + "...");
+	System.out.println(pile);
 	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+	System.out.println("      1\n     /\\\n    4 2\n   /\\ /\\\n  5 7 6 3\n /\\/\n8 7 10");
     }//end main()
 
 }//end class ALHeap
